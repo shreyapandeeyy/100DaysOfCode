4f832b49868a11fbf5d404d2f758cd7b3866e376
shreyapandeeyy <130491256+shreyapandeeyy@users.noreply.github.com>
2024-03-28 22:35:33 +0530

    Add files via upload

---
+++ Day86.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        List<Pair<Integer, Integer>> list = new ArrayList<>();
+        for (int i = 0; i < mat.length; i++) {
+            int count = 0;
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] == 1) {
+                    count++;
+                }
+            }
+            list.add(new Pair<>(count, i));
+        }
+        Collections.sort(list, (a, b) -> a.getKey() - b.getKey());
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = list.get(i).getValue();
+        }
+        return result;
+    }
+}
