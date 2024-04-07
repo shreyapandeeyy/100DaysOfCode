1ee1882b7985c2ee982a23e243a842ef21b9cece
shreyapandeeyy <130491256+shreyapandeeyy@users.noreply.github.com>
2024-04-07 22:43:53 +0530

    Add files via upload

---
+++ Day97.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pairSum(ListNode head) {
+    Stack<ListNode> stack=new Stack<>();
+    ListNode dummy=head;
+    while(dummy!=null){
+        stack.push(dummy);
+        dummy=dummy.next;
+    }
+    int max=0;
+    while(stack.size()>stack.size()/2){
+        max=Math.max(max,head.val+stack.pop().val);
+        head=head.next;
+    }
+    return max;
+    }
+}
